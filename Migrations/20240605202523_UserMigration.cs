56b0022b8a16bccb63881ef6bc92965010c27c8e
Gustavo René <renegustavo42@gmail.com>
2024-06-05 17:25:56 -0300

    feat: Create migration for User

---
+++ Migrations/20240605202523_UserMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectFor7COMm.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBC_User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Username = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    PasswordSalt = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBC_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBC_User");
+        }
+    }
+}
