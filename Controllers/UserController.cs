725a5311660919af9e48c38f0ea155433e9fab70
Gustavo René <renegustavo42@gmail.com>
2024-06-05 17:05:00 -0300

    feat: Create Controller for User

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectFor7COMm.Services;
+
+namespace ProjectFor7COMm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+    }
+}
